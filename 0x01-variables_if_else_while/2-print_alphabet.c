ba9aac4dceb2f06ca55a8e80c535ad1588bb4f28
NEMac97 <ezekponnaemeka@gmail.com>
2022-10-17 16:48:27 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always Zero (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+	
