90cc4bcff8abf83ed6d535c7b3f11d66d402c7c2
NEMac97 <ezekponnaemeka@gmail.com>
2022-10-21 21:17:44 -0700

    debugging tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- test function that indicates if an integer is
+ * positive or negative
+ *
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
